9ca820b9d5010a3822b41d4a54df1baa87755ebd
Spencer Hill <spencerahill@gmail.com>
2020-03-05 14:10:07 -0500

    Use minor ticks for lat and sinlat xaxis; other plot tweaks

--- puffins/plotting.py
+++ puffins/plotting.py
@@ -36,17 +36,20 @@ def _left_bottom_spines_only(ax, displace=False):
     ax.yaxis.set_ticks_position('left')
 
 
-def sinlat_xaxis(ax, start_lat=0, end_lat=90):
+def sinlat_xaxis(ax, start_lat=-90, end_lat=90):
     """Make the x-axis be in sin of latitude."""
     ax.set_xlim([sindeg(start_lat), sindeg(end_lat)])
-    ax.set_xticks(sindeg(np.arange(start_lat, end_lat + 1, 10)))
     if start_lat == 0 and end_lat == 90:
-        ax.set_xticklabels(['EQ', '', '', r'30$^\circ$', '', '',
-                            r'60$^\circ$', '', '', r'90$^\circ$'])
+        ax.set_xticks(sindeg([0, 30, 60, 90]))
+        ax.set_xticks(sindeg([10, 20, 40, 50, 70, 80]), minor=True)
+        ax.set_xticklabels(['EQ', r'30$^\circ$' r'60$^\circ$', r'90$^\circ$'])
     elif start_lat == -90 and end_lat == 90:
-        ax.set_xticklabels(['90' + _DEGR_S, '', '', '', '', '', '30' + _DEGR_S,
-                            '', '', 'EQ', '', '', '30' + _DEGR_N, '', '', '',
-                            '', '', '90' + _DEGR_N])
+        ax.set_xticks(sindeg([-90, -60, -30, 0, 30, 60, 90]))
+        minorticks = [-80, -70, -50, -40, -20, -10,
+                      10, 20, 40, 50, 70, 80]
+        ax.set_xticks(sindeg(minorticks), minor=True)
+        ax.set_xticklabels(['90' + _DEGR_S, " ", '30' + _DEGR_S, 'EQ',
+                            '30' + _DEGR_N, " ", '90' + _DEGR_N])
 
 
 def lat_xaxis(ax, start_lat=-90, end_lat=90):
@@ -121,7 +124,7 @@ def _plot_cutoff_ends(lats, arr, ax=None, **kwargs):
     ax.plot(lats[2:-2], arr[2:-2], **kwargs)
 
 
-def panel_label(ax, panel_num, x=0.03, y=0.95, extra_text=None,
+def panel_label(ax, panel_num, x=0.03, y=0.9, extra_text=None,
                 **text_kwargs):
     letters = 'abcdefghijklmnopqrstuvwxyz'
     label = '({})'.format(letters[panel_num])
@@ -131,15 +134,15 @@ def panel_label(ax, panel_num, x=0.03, y=0.95, extra_text=None,
 
 
 @default_gca
-def mark_x0(ax=None, linestyle='--', color='0.5', **kwargs):
+def mark_x0(ax=None, linewidth=0.5, color='0.5', **kwargs):
     """Mark the x intercept line on the given axis."""
-    return ax.axvline(x=0, linestyle=linestyle, color=color, **kwargs)
+    return ax.axvline(x=0, linewidth=linewidth, color=color, **kwargs)
 
 
 @default_gca
-def mark_y0(ax=None, linestyle='--', color='0.5', **kwargs):
+def mark_y0(ax=None, linewidth=0.5, color='0.5', **kwargs):
     """Mark the y intercept on the given axis."""
-    return ax.axhline(y=0, linestyle=linestyle, color=color, **kwargs)
+    return ax.axhline(y=0, linewidth=linewidth, color=color, **kwargs)
 
 
 @default_gca
