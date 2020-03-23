1f28c810c5538cba122eda45589361bbb157ba74
Spencer Hill <spencerahill@gmail.com>
2020-03-23 08:44:36 -0400

    Simplify logic in interp.interp_ds_sigam_to_p

--- puffins/interp.py
+++ puffins/interp.py
@@ -79,17 +79,19 @@ def interpolate(x, y, x_target, dim):
     return y_target
 
 
-def interp_ds_sigma_to_p(ds, num_p, method="cubic", p_sfc_str=P_SFC_STR,
+def interp_ds_sigma_to_p(ds, plevs, method="cubic", p_sfc_str=P_SFC_STR,
                          sigma_str=SIGMA_STR, lev_str=LEV_STR,
                          lat_str=LAT_STR):
     """Interpolate Dataset with sigma corodinates to uniform pressures."""
-    p_sfc = ds[p_sfc_str]
-    p_sfc_min = float(ds[sigma_str].max()*p_sfc.min())
-    p_top_max = float(ds[sigma_str].min()*p_sfc.max())
-    p_fixed_vals = np.linspace(p_top_max, p_sfc_min, num_p)
+    if isinstance(plevs, int):
+        p_sfc_min = 1000.e2  # float(ds[sigma_str].max()*p_sfc.min())
+        p_top_max = 10.e2  # float(ds[sigma_str].min()*p_sfc.max())
+        p_fixed_vals = np.linspace(p_top_max, p_sfc_min, plevs)
+    else:
+        p_fixed_vals = plevs
     p_fixed = xr.DataArray(p_fixed_vals, dims=[sigma_str],
                            coords={sigma_str: p_fixed_vals}, name="p")
-    sigma_fixed = p_fixed / p_sfc
+    sigma_fixed = p_fixed / ds[p_sfc_str]
 
     interped = []
     lats = ds[lat_str]
