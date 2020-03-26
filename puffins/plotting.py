9b06e55f4e80768f3aa94647effdd1702d3773c6
Spencer Hill <spencerahill@gmail.com>
2020-03-26 16:04:29 -0400

    Make 1-1 line less obtrusive

--- puffins/plotting.py
+++ puffins/plotting.py
@@ -15,6 +15,9 @@ _DEGR_S = _DEGR + 'S'
 _DEGR_N = _DEGR + 'N'
 
 
+PlotArr = namedtuple('PlotArr', ['func', 'label', 'plot_kwargs'])
+
+
 plt_rc_params_custom = {
     "axes.spines.top": False,  # Turn off top spine in plots.
     "axes.spines.right": False,  # Turn off right spine in plots.
@@ -167,8 +170,8 @@ def mark_y0(ax=None, linewidth=0.5, color='0.5', **kwargs):
 
 
 @default_gca
-def mark_one2one(ax=None, *line_args, linestyle='--', color='0.5',
-                 **line_kwargs):
+def mark_one2one(ax=None, *line_args, linestyle=':', color='0.7',
+                 linewidth=0.8, **line_kwargs):
     """Mark the identity line, y=x, on the given axis.
 
     From https://stackoverflow.com/a/28216751/1706640.
@@ -189,9 +192,6 @@ def mark_one2one(ax=None, *line_args, linestyle='--', color='0.5',
     ax.callbacks.connect('ylim_changed', callback)
     return ax
 
-
-
-PlotArr = namedtuple('PlotArr', ['func', 'label', 'plot_kwargs'])
 
 if __name__ == '__main__':
     pass
