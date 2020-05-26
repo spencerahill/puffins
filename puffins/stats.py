dbdc978b95142ce0f640a08dcdb9de53274436d8
Spencer Hill <shill@berimbau.ldeo.columbia.edu>
2020-05-26 16:22:49 -0400

    Add option to just return slope and intercept in trend

--- puffins/stats.py
+++ puffins/stats.py
@@ -3,10 +3,12 @@ import numpy as np
 import xarray as xr
 
 
-def trend(arr, dim, order=1):
+def trend(arr, dim, order=1, ret_slope_y0=False):
     """Compute linear or higher-order polynomial fit."""
     coord = arr.coords[dim]
     slope, y0 = np.polyfit(coord, arr, order)
+    if ret_slope_y0:
+        return slope, y0
     return xr.ones_like(arr)*np.polyval([slope, y0], coord)
 
 
