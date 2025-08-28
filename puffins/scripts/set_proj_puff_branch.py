2bd6d8baa5d4b14c3577db794a101030de53924d
Spencer Hill <shill1@ccny.cuny.edu>
2025-08-28 11:47:56 -0400

    bring setup/install machinery up to date with modern build systems

--- puffins/scripts/set_proj_puff_branch.py
+++ puffins/scripts/set_proj_puff_branch.py
@@ -1,20 +1,20 @@
 #! /usr/bin/env python
 """Switch puffins to the git branch corresponding to a particular project."""
 import argparse
-
 from puffins.nb_utils import setup_puffins
 
-
-if __name__ == "__main__":
+def main(argv=None):
     parser = argparse.ArgumentParser(
-        description="Checkout a particular projects' puffins branch")
+        description="Checkout a particular project's puffins branch"
+    )
     parser.add_argument(
         "branch_name",
-        action="store",
         nargs="?",
         default="master",
-        type=str,
         help="Name of the puffins git branch to be checked out.",
     )
-    branch_name = parser.parse_args().branch_name
-    setup_puffins(branch_name)
+    args = parser.parse_args(argv)
+    setup_puffins(args.branch_name)
+
+if __name__ == "__main__":
+    main()
